f9f5e1d77938b62eea58db1add12189f2c7712e5
NUSAKU <96153429+oxygenerator-nusaku@users.noreply.github.com>
2021-12-30 01:16:08 +0700

    Create Team Component

---
+++ components/teams.js
@@ -0,0 +1,119 @@
+import Image from "next/image";
+import React from "react";
+import Container from "./container";
+
+import userTwoImg from "../public/img/user2.jpg";
+import userFourImg from "../public/img/user4.jpg";
+import userFiveImg from "../public/img/user5.jpg";
+
+export default function Testimonials() {
+  return (
+    <Container>
+      <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-3">
+        <div className="lg:col-span-2 xl:col-auto">
+          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
+            <Avatar
+              image={userTwoImg}
+              name="Yohanis Cianes Walean"
+              title="CEO NUSAKU"
+            />
+
+            <div className="flex justify-end w-full h-full bg-gray-100 px-14 rounded-2xl py-4 dark:bg-trueGray-500">
+              <div className="px-4">
+                <a href="https://id.linkedin.com/in/yohanis-walean-a297713b" target="_blank" rel="noopener">
+                  <span className="sr-only">LinkedIn</span>
+                  <LinkedIn />
+                </a>
+              </div>
+              <div className="px-4">
+                <a href="https://https://facebook.com/YohanisCianesWalean" target="_blank" rel="noopener">
+                  <span className="sr-only">Facebook</span>
+                  <Facebook />
+                </a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div className="">
+          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
+            <Avatar
+              image={userFourImg}
+              name="Pradana Aria Putrantau"
+              title="Lead marketing"
+            />
+            <div className="flex justify-end w-full h-full bg-gray-100 px-14 rounded-2xl py-4 dark:bg-trueGray-500">
+              <div className="px-4">
+                <a href="https://id.linkedin.com/in/pradana-aria-7830661a0" target="_blank" rel="noopener">
+                  <span className="sr-only">LinkedIn</span>
+                  <LinkedIn />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="">
+          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
+            <Avatar
+              image={userFiveImg}
+              name="Ricko"
+              title="Digital Marketing"
+            />
+            <div className="flex justify-end w-full h-full bg-gray-100 px-14 rounded-2xl py-4 dark:bg-trueGray-500 text-gray-400">
+              <div className="px-4">
+                <a href="https://www.linkedin.com/in/ricko-enggita-putra-303535229" target="_blank" rel="noopener">
+                  <span className="sr-only">LinkedIn</span>
+                  <LinkedIn />
+                </a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </Container>
+  );
+}
+
+const LinkedIn = ({ size = 24 }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
+);
+const Facebook = ({ size = 24 }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.325v-21.35c0-.732-.593-1.325-1.325-1.325z" /></svg>
+);
+const Instagram = ({ size = 24 }) => (
+
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11.984 16.815c2.596 0 4.706-2.111 4.706-4.707 0-1.409-.623-2.674-1.606-3.538-.346-.303-.735-.556-1.158-.748-.593-.27-1.249-.421-1.941-.421s-1.349.151-1.941.421c-.424.194-.814.447-1.158.749-.985.864-1.608 2.129-1.608 3.538 0 2.595 2.112 4.706 4.706 4.706zm.016-8.184c1.921 0 3.479 1.557 3.479 3.478 0 1.921-1.558 3.479-3.479 3.479s-3.479-1.557-3.479-3.479c0-1.921 1.558-3.478 3.479-3.478zm5.223.369h6.777v10.278c0 2.608-2.114 4.722-4.722 4.722h-14.493c-2.608 0-4.785-2.114-4.785-4.722v-10.278h6.747c-.544.913-.872 1.969-.872 3.109 0 3.374 2.735 6.109 6.109 6.109s6.109-2.735 6.109-6.109c.001-1.14-.327-2.196-.87-3.109zm2.055-9h-12.278v5h-1v-5h-1v5h-1v-4.923c-.346.057-.682.143-1 .27v4.653h-1v-4.102c-1.202.857-2 2.246-2 3.824v3.278h7.473c1.167-1.282 2.798-2 4.511-2 1.722 0 3.351.725 4.511 2h7.505v-3.278c0-2.608-2.114-4.722-4.722-4.722zm2.722 5.265c0 .406-.333.735-.745.735h-2.511c-.411 0-.744-.329-.744-.735v-2.53c0-.406.333-.735.744-.735h2.511c.412 0 .745.329.745.735v2.53z" /></svg>
+);
+
+function Avatar(props) {
+  return (
+    <div className="flex items-center mt-8 space-x-3">
+      <div className="flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
+        <Image
+          src={props.image}
+          width="40"
+          height="40"
+          alt="Avatar"
+          layout="responsive"
+          placeholder="blur"
+        />
+      </div>
+      <div>
+        <div className="text-lg font-medium">{props.name}</div>
+        <div className="text-gray-600 dark:text-gray-400">{props.title}</div>
+      </div>
+    </div>
+  );
+}
+
+function Mark(props) {
+  return (
+    <>
+      {" "}
+      <mark className="text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">
+        {props.children}
+      </mark>{" "}
+    </>
+  );
+}
